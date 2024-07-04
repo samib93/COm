62c081335a0a1f4acd2f0860abfae53887b7bc65
alioune badara fall <aliounebadarafall01@gmail.com>
2024-07-04 19:19:26 +0200

    last version

--- src/Component/Page/Partanariat/Partanariat.js
+++ src/Component/Page/Partanariat/Partanariat.js
@@ -7,6 +7,10 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
 import ImgSignup from '../../../assets/img/partanariat.png'
 import Logo from '../../../assets/img/logohead.png'
+import facebook from "../../../assets/img/logo_facebook.png";
+import Instagram from '../../../assets/img/logo_instagram.png';
+import linkedin from '../../../assets/img/logo_likendin.png';
+import Twitter from '../../../assets/img/logo_Twitter.png';
 
 const Partanariat = () => {
   const [producteur, setProducteur] = useState('');
@@ -181,7 +185,56 @@ const Partanariat = () => {
           <img src={ImgSignup} alt="Plantation" className="object-cover rounded-lg shadow-lg" />
         </div>
       </div>
+      <footer className="bg-black text-white py-8">
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
+            <div className="mb-6 md:mb-0">
+              <h2 className="font-florensa text-3xl font-normal mb-4">Assistance</h2>
+              <ul>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">FAQ</a></li>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">Contactez-nous</a></li>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">Support technique</a></li>
+              </ul>
+            </div>
+            <div className="mb-6 md:mb-0">
+              <h2 className="font-florensa text-3xl font-normal mb-4">À propos de</h2>
+              <ul>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">À propos de nous</a></li>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">Politique de confidentialité</a></li>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">Politique en matière de cookies</a></li>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">Règlement général sur la protection des données</a></li>
+              </ul>
+            </div>
+            <div className="mb-6 md:mb-0">
+              <h2 className="font-florensa text-3xl font-normal mb-4">Conditions</h2>
+              <ul>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">Conditions générales</a></li>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">Conditions de facturation</a></li>
+                <li><a href="#" className="font-raleway text-gray-400 hover:text-white">Politique de remboursement</a></li>
+              </ul>
+            </div>
+            <div className="flex space-x-4 w-32 h-32">
+              <a href="#" className="mt-2 text-gray-400 hover:text-white">
+                <img src={facebook} alt="Facebook" />
+              </a>
+              <a href="#" className="mt-2 text-gray-400 hover:text-white">
+                <img src={Twitter} alt="Twitter" />
+              </a>
+              <a href="#" className="mt-2 text-gray-400 hover:text-white">
+                <img src={Instagram} alt="Instagram" />
+              </a>
+              <a href="#" className="mt-2 text-gray-400 hover:text-white">
+                <img src={linkedin} alt="LinkedIn" />
+              </a>
+            </div>
+          </div>
+          <div className="text-center text-gray-400 mt-8">
+            &copy; 2024 FRESH. Tous droits réservés.
+          </div>
+        </div>
+      </footer>
     </div>
+    
   );
 };
 
